2868e9b47868f8c03d815d1c0374c9c288ac86cd
yotaro-shimose <yosemat.beta@gmail.com>
2026-04-12 11:28:40 +0000

    css

--- graphvis/src/components/GraphCanvas.tsx
+++ graphvis/src/components/GraphCanvas.tsx
@@ -77,7 +77,7 @@ export const GraphCanvasComponent: React.FC = () => {
   }
 
   return (
-    <div style={{ position: 'fixed', top: 0, left: 0, width: '100vw', height: '100vh', background: '#000' }}>
+    <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', background: '#000' }}>
       <ControlPanel
         experiments={experiments}
         selectedExperiment={selectedExperiment}
